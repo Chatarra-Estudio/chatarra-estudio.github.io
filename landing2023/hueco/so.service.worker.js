069b87571a4e397b47bddfd293f756b28f138b26
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2023-10-07 20:51:45 +0200

    probando

--- landing2023/hueco/so.service.worker.js
+++ landing2023/hueco/so.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1696703106|645777405";
+const CACHE_VERSION = "1696704427|26577703";
 const CACHE_PREFIX = "Hueco-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "so.offline.html";
